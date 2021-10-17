c2201bca5dfe2b702f21dcef00aba5962237400a
Jack14188 <jackjaros06@gmail.com>
2021-10-16 19:25:00 -0500

    Turret Testing, getting test hardware correct

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHub/TestHubHardware.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.TestHub;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class TestHubHardware {
+    // Motors and Servos
+    public DcMotor Motor1;
+    public DcMotor Motor2;
+    public DcMotor Motor3;
+    public DcMotor Motor4;
+    public CRServo CRServo1;
+    public CRServo CRServo2;
+    public AnalogInput PivotPT;
+
+    HardwareMap TestHubHardware;
+
+    public void init(HardwareMap TestHubHardware){
+
+        // Define motors and servos
+        Motor1 = TestHubHardware.get(DcMotor.class, "Motor1");
+        Motor2 = TestHubHardware.get(DcMotor.class, "Motor2");
+        Motor3 = TestHubHardware.get(DcMotor.class, "Motor3");
+        Motor4 = TestHubHardware.get(DcMotor.class, "Motor4");
+        CRServo1 = TestHubHardware.get(CRServo.class, "CRServo1");
+        CRServo2 = TestHubHardware.get(CRServo.class, "CRServo2");
+        PivotPT = TestHubHardware.get(AnalogInput.class, "PivotPT");
+        // ColorSensor1 = JustmotorhardwareMap.get(NormalizedColorSensor.class, "ColorSensor1");
+
+        //servo = hardwareMap.get(Servo.class, "servo");
+
+        // Set all motors to zero power
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        Motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Motor3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Motor4.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Motor4.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHub/TurretTest.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode.TestHub;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TurretTest extends LinearOpMode {
+    TestHubHardware robot = new TestHubHardware();
+
+    double extendMin = 2, extendMax = 1300, extendSet = extendMin;
+    double extendDifference = 0, extendMultiplied = 0, extendP = -.02, extendD = 0;
+
+
+    double vPivotMin = 0.8, vPivotMax = 3, vPivotSet = 1.15;
+    double vPivotDifference = 0, vPivotMultiplied = 0, vPivotP = 3, vPivotD = 0;
+
+    double rotateMin = -270, rotateMax = 270, rotateSet = 0;
+    double rotateDifference = 0, rotateMultiplied = 0, rotateP = .0001, rotateD = 0;
+
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()) {
+            //Linear Slides Extend
+
+            extendSet = extendSet + (15 * (gamepad1.left_stick_x));
+            vPivotSet = vPivotSet + (.02 * gamepad1.right_stick_y);
+            rotateSet = rotateSet + (.001 * gamepad1.right_stick_x);
+            //Setpoint limits
+            if(extendSet < extendMin){
+                extendSet = (extendMin + 2);
+            }else  if( extendSet > extendMax){
+                extendSet = (extendMax - 2);
+            }
+
+            extendDifference = robot.Motor1.getCurrentPosition() - extendSet;
+            extendMultiplied = extendDifference * extendP;
+
+            //VPivot Limits
+            if(vPivotSet < vPivotMin){
+                vPivotSet = vPivotMin;
+            }else if(vPivotSet > vPivotMax){
+                vPivotSet = vPivotMax;
+            }
+
+            vPivotDifference = robot.PivotPT.getVoltage() - vPivotSet;
+            vPivotMultiplied = vPivotDifference * vPivotP;
+
+
+            //Hpivot Limits
+            if(rotateSet < rotateMin){
+                rotateSet = rotateMin;
+            }else if(rotateSet > rotateMax){
+                rotateSet = rotateMax;
+            }
+
+            rotateDifference = robot.Motor3.getCurrentPosition() - rotateSet;
+            rotateMultiplied = rotateDifference * rotateP;
+
+            robot.Motor1.setPower(extendMultiplied);
+            robot.Motor2.setPower(vPivotMultiplied);/*
+            robot.Motor3.setPower(rotateMultiplied);
+            */
+
+            telemetry.addData("vPivotSet", vPivotSet);
+            telemetry.addData("rotateSet", rotateSet);
+            telemetry.addData("extendSet", extendSet);
+            telemetry.addData("motor1", robot.Motor1.getPower());
+            telemetry.addData("extendcurrent", robot.Motor1.getCurrentPosition());
+            telemetry.addData("rotate power", rotateMultiplied);
+            telemetry.addData("Vpivot power", vPivotMultiplied);
+            telemetry.addData("extend power", extendMultiplied);
+            telemetry.addData("pot", robot.PivotPT.getVoltage());
+            telemetry.update();
+        }
+    }
+}
