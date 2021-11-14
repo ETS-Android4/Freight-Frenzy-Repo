a587a5140dd2941448847a4f361e5a30c2359761
Ethan <ethantheavocado@gmail.com>
2021-11-14 17:48:06 -0600

    robot is woroking drivebases

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHub/HomingProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHub/HomingProgram.java
@@ -18,15 +18,20 @@ public class HomingProgram extends LinearOpMode {
         while(opModeIsActive()){
 
             robot.TP_M.setPower(VPivotClass.VPivotAutoMethod(1.15,.5, robot.TP_P.getVoltage()));
+            //robot.TE_M.setPower(ExtendClass.ExtendAutoMethod(100, .5, robot.TE_M.getCurrentPosition(), robot.TE_G.getState()));
             if(robot.TP_P.getVoltage() > 1.1 && robot.TP_P.getVoltage() < 1.25){
-                ExtendClass.ExtendHoming(robot.TE_G.getState(), robot.TE_M.getCurrentPosition());
-                robot.TE_M.setPower(ExtendClass.ExtendMotorPowerReturn());
+                telemetry.addData("in extend homing",1);
+               // ExtendClass.ExtendHoming(robot.TE_G.getState(), robot.TE_M.getCurrentPosition());
+                robot.TE_M.setPower(ExtendClass.ExtendHoming(robot.TE_G.getState(), robot.TE_M.getCurrentPosition()));
                 /*if(ExtendClass.isHomedExtendReturn() == true){
                     RotateClass.RotateHoming(robot.TR_G.getState(), robot.TR_M.getCurrentPosition());
                 }*/
             }
 
+            telemetry.addData("mag sensor", robot.TE_G.getState());
+            telemetry.addData("homiing next set", ExtendClass.HomingnextSetReturn());
             telemetry.addData("POT reading", robot.TP_P.getVoltage());
+            telemetry.addData("extend mp", robot.TE_M.getPower());
             telemetry.addData("EXtend is homed", ExtendClass.isHomedExtendReturn());
             telemetry.addData("rotate is homed", RotateClass.isHomedRotateReturn());
 
