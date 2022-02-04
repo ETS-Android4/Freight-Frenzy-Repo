7a0b121ce78a4337fb8582cc0801bcc53d69f419
Ethan <ethantheavocado@gmail.com>
2022-02-04 17:03:00 -0600

    OPEN CV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHub/OpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHub/OpenCVTest.java
@@ -80,13 +80,13 @@ public class OpenCVTest extends LinearOpMode {
         static final Scalar CYAN = new Scalar(0, 139, 139);
 
         //sets the boxes where we will look
-        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(155, 260);
+        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(155, 220);
         static final int REGION1_WIDTH = 60;
         static final int REGION1_HEIGHT = 25;
-        static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(270, 260);
+        static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(270, 220);
         static final int REGION2_WIDTH = 60;
         static final int REGION2_HEIGHT = 25;
-       static final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(430, 260);
+       static final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(430, 220);
         static final int REGION3_WIDTH = 60;
         static final int REGION3_HEIGHT = 25;
         //static final Point REGION4_TOPLEFT_ANCHOR_POINT = new Point(100, 70);
@@ -111,6 +111,7 @@ public class OpenCVTest extends LinearOpMode {
         Mat HSV = new Mat();
         Mat G = new Mat();
         Mat H = new Mat();
+        Mat GRAY = new Mat();
         int avg1, avg2, avg3, avg4;
 
         /*
@@ -119,8 +120,8 @@ public class OpenCVTest extends LinearOpMode {
          */
         //actual image processing
         void inputToG(Mat input) {
-            Imgproc.cvtColor(input, RGBA, Imgproc.COLOR_RGB2RGBA);
-            Core.extractChannel(RGBA, G, 1);
+            Imgproc.cvtColor(input, RGBA, Imgproc.COLOR_RGB2GRAY);
+            Core.extractChannel(GRAY, GRAY, 1);
 
         }
 
@@ -128,9 +129,9 @@ public class OpenCVTest extends LinearOpMode {
         public void init(Mat firstFrame) {
             inputToG(firstFrame);
             //sets region to look in for color
-            region1_G = G.submat(new Rect(region1_pointA, region1_pointB));
-            region2_G = G.submat(new Rect(region2_pointA, region2_pointB));
-            region3_G = G.submat(new Rect(region3_pointA, region3_pointB));
+            region1_G = GRAY.submat(new Rect(region1_pointA, region1_pointB));
+            region2_G = GRAY.submat(new Rect(region2_pointA, region2_pointB));
+            region3_G = GRAY.submat(new Rect(region3_pointA, region3_pointB));
             //region4_G = L.submat(new Rect(region4_pointA, region4_pointB));
         }
 
